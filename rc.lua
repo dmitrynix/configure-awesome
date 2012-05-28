727d8f36ef42a4e88dbe3e1b126a56e3ed7accdd
Dmitry Nix <dmitry.nix@gmail.com>
2012-05-28 09:17:55 -0300

    Add themes and use dust theme

--- rc.lua
+++ rc.lua
@@ -54,7 +54,8 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+-- beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init("/home/dmitry/.config/awesome/themes/dust/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "terminator"
