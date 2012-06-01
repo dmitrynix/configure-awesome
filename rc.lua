ec8969364f033892ff1227ffec8ce30c46bdb3fc
Dmitry Nix <dmitry.nix@gmail.com>
2012-06-01 20:07:54 -0300

    Fix volumewidget position

--- rc.lua
+++ rc.lua
@@ -214,8 +214,8 @@ for s = 1, screen.count() do
       mylayoutbox[s],
       mytextclock,
       s == 1 and mysystray or nil,
-      volumewidget,
       mytasklist[s],
+      volumewidget,
       layout = awful.widget.layout.horizontal.rightleft
   }
 end
